738f1acca880c2b1009c0c785953dc334785d2a0
qzink <101195095+Qzincs@users.noreply.github.com>
2025-01-04 15:41:56 +0800

    add time.txt; format xargs; fix pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -12,16 +12,16 @@ main(int argc, char *argv[])
     pipe(p1);
     pipe(p2);
 
-    char buf[1];
+    char buf;
 
     if(fork()==0) {
-        read(p1[0], buf, 1);
+        read(p1[0], &buf, 1);
         int pid = getpid();
         printf("%d: received ping\n", pid);
         write(p2[1], "B", 1);
     } else {
         write(p1[1], "A", 1);
-        read(p2[0], buf, 1);
+        read(p2[0], &buf, 1);
         int pid = getpid();
         printf("%d: received pong\n", pid);
     }
