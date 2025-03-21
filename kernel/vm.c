30ce333b38c4fa9d2bfd14c9d08c679eec0d8fa1
qzink <101195095+Qzincs@users.noreply.github.com>
2025-03-21 17:34:19 +0800

    Lazy allocation

--- kernel/vm.c
+++ kernel/vm.c
@@ -183,7 +183,7 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
     if((pte = walk(pagetable, a, 0)) == 0)
       panic("uvmunmap: walk");
     if((*pte & PTE_V) == 0)
-      panic("uvmunmap: not mapped");
+      continue;
     if(PTE_FLAGS(*pte) == PTE_V)
       panic("uvmunmap: not a leaf");
     if(do_free){
@@ -439,4 +439,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+void
+vmprint_recursive(pagetable_t pagetable, int level) {
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      for(int j = 0; j < level; j++) {
+        printf(".. ");
+      }
+      uint64 child = PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      // 如果是非叶子节点继续递归
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        vmprint_recursive((pagetable_t)child, level + 1);
+    }
+  }
+}
+
+void 
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable); 
+  vmprint_recursive(pagetable, 0);
 }
